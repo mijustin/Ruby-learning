9ef56bf173ba26801f51dde4e0803b894ed1af47
Justin Jackson <justin@mailoutinteractive.com>
2012-02-16 22:16:15 -0700

    changed code as per brendan tips

--- favnum.rb
+++ favnum.rb
@@ -1,30 +1,28 @@
 puts 'What is your favorite number?'
-puts ''
+puts 
 number = gets.chomp
-puts ''
+puts 
 puts 'I like the number ' + number + '!'
-puts ''
+puts 
 puts 'There is a bigger and better favorite number!'
-puts ''
+puts 
 puts 'Presenting number.......'
-puts ''
+puts 
 puts number.to_i + 1 
-puts ''
+puts 
 puts 'Do you like this number?'
-puts ''
+puts 
 response = gets.chomp
 if response == 'yes'
-puts ''
+puts 
 puts 'YES! This is an excellent number'
-puts ''
-end
-if response == 'no'
-puts ''
+puts
+elsif response == 'no'
+puts 
 puts 'DAMN! I thought it was an excellent number'
-puts ''
-end
-if response != 'yes' and response!= 'no'
-puts ''
+puts 
+elsif response != 'no' or response != 'yes'
+puts 
 puts 'Whatever'
-puts ''
+puts 
 end
